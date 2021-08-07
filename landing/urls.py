a81414e03469da02e4e1c04b2ff2e04a9d0c582b
cleonst427 <cleons@pucp.edu.pe>
2021-08-07 18:30:52 -0500

    Initial

---
+++ landing/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+
+from landing import views
+
+urlpatterns = [
+    path('home/', views.renderHome, name="home"),
+    path('about/', views.renderAbout, name="about"),
+    path('short_bio/', views.renderShortBio, name="short_bio"),
+    path('resume/', views.renderResume, name="resume"),
+    path('research/', views.renderResearch, name="research"),
+    path('projects/', views.renderProjects, name="projects"),
+    path('contact/', views.renderContact, name="contact"),
+    path('article_list/', views.renderArticle, name="article_list"),
+    path('article_detail/<int:pk>/', views.article_detail, name='article_detail'),
+    path('contactanos/', views.renderContactUs_email, name="contact_us"),
+]
